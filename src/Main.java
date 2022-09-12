e7ce7040a4c5d7015a2668c44a5c941251ff167e
LucasFA94 <lucasfernandesafonso@gmail.com>
2022-09-12 20:50:06 -0300

    operação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a;
+        int b = 2;
+        a = 3;
+
+        int soma = a + b;
+        int subtracaoo = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / (float) b;
+
+        System.out.println(soma);
+        System.out.println(subtracaoo);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
     }
 }
